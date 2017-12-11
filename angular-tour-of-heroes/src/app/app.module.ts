a121d33d99605f381e2e0b3fa11e82457c187460
moueza <mouezapeter@gmail.com>
2017-12-11 03:29:44 +0100

    tutorial5 sov

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -6,7 +6,8 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 // import { HeroComponent } from './hero/hero.component';
 
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component'; // <-- NgModel lives here
 
 
 
@@ -14,6 +15,7 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
   declarations: [
     AppComponent,
     HeroesComponent,
+    HeroDetailComponent,
    // HeroComponent
   ],
   imports: [
