73b365d0fbdabc76694ce569a79f2da2427970cc
moueza <mouezapeter@gmail.com>
2017-12-15 19:01:07 +0100

    Heroes and HTTP

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -10,11 +10,14 @@ import { of } from 'rxjs/observable/of';
 import { MessageService } from './message.service';
 
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+	      private messageService: MessageService,
+	      private http: HttpClient) { }
 
 
  getHeroes(): Observable<Hero[]> {
@@ -30,5 +33,13 @@ export class HeroService {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
   }
+  
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add('HeroService: ' + message);
+  }
+  
+  private heroesUrl = 'api/heroes';  // URL to web api
+
 }
 
