cf57a79e9beb4671b45f66bb2c2a32905bdf13fd
moueza <mouezapeter@gmail.com>
2017-12-15 11:22:34 +0100

    Add a hero detail route

--- angular-tour-of-heroes/src/app/app-routing.module.ts
+++ angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -5,11 +5,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent }   from './dashboard/dashboard.component';
 
 import { HeroesComponent }      from './heroes/heroes.component';
-
+import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 const routes: Routes = [
 			{ path: 'heroes', component: HeroesComponent },
 			{ path: 'dashboard', component: DashboardComponent },
-			{ path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+			{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+			{ path: 'detail/:id', component: HeroDetailComponent }
 			];
 
 
