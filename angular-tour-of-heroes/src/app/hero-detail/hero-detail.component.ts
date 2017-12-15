c2c6892eaf71f2ed8733809a1bfd5dad706fda32
moueza <mouezapeter@gmail.com>
2017-12-15 12:01:45 +0100

    Extract the id route parameter  ERROR in src/app/hero-detail/hero-detail.component.ts(32,20): error TS2551: Property 'getHero' does not exist on type 'HeroService'. Did you mean 'getHeroes'?

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -23,7 +23,14 @@ export class HeroDetailComponent implements OnInit {
   private heroService: HeroService,
   private location: Location) { }
 
-    ngOnInit() {
+ ngOnInit() : void {
+       this.getHero();
     }
+
+    getHero(): void {
+  const id = +this.route.snapshot.paramMap.get('id');
+  this.heroService.getHero(id)
+    .subscribe(hero => this.hero = hero);
+}
 
 }
