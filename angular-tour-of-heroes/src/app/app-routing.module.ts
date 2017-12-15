e0fd41563bba67a919feac1abc0f212e4784fdfe
moueza <mouezapeter@gmail.com>
2017-12-15 11:05:24 +0100

    Add a default route

--- angular-tour-of-heroes/src/app/app-routing.module.ts
+++ angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -8,7 +8,8 @@ import { HeroesComponent }      from './heroes/heroes.component';
 
 const routes: Routes = [
 			{ path: 'heroes', component: HeroesComponent },
-{ path: 'dashboard', component: DashboardComponent }
+			{ path: 'dashboard', component: DashboardComponent },
+			{ path: '', redirectTo: '/dashboard', pathMatch: 'full' }
 			];
 
 
