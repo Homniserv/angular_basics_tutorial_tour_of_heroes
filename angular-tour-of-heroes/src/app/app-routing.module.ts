185675d23376e9287536878223d42c497ce5c486
moueza <mouezapeter@gmail.com>
2017-12-15 11:01:05 +0100

    Add the dashboard route

--- angular-tour-of-heroes/src/app/app-routing.module.ts
+++ angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 //import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
-
+import { DashboardComponent }   from './dashboard/dashboard.component';
 
 import { HeroesComponent }      from './heroes/heroes.component';
 
 const routes: Routes = [
-  { path: 'heroes', component: HeroesComponent }
-];
+			{ path: 'heroes', component: HeroesComponent },
+{ path: 'dashboard', component: DashboardComponent }
+			];
 
 
 
