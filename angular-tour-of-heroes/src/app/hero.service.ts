29d66067e4e20967a95eb0060b2ce72255507f04
moueza <mouezapeter@gmail.com>
2017-12-16 00:57:09 +0100

    KO...no back button any more

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -21,16 +21,16 @@ export class HeroService {
 	      private messageService: MessageService,
 	      private http: HttpClient) { }
 
-/** GET heroes from the server */
+  /** GET heroes from the server */
  getHeroes(): Observable<Hero[]> {
     // Todo: send the message _after_ fetching the heroes
     // this.messageService.add('HeroService: fetched heroes');
     //return of(HEROES);
     return this.http.get<Hero[]>(this.heroesUrl)
       .pipe(
-      catchError(this.handleError('getHeroes', []))
-    );
-     }
+	    catchError(this.handleError('getHeroes', []))
+	    );
+  }
 
 
 
@@ -47,5 +47,29 @@ export class HeroService {
   
   private heroesUrl = 'api/heroes';  // URL to web api
 
+
+
+
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 }
+
 
